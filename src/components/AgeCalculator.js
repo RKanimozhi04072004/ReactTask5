9a999cdfc2969566b9307a1bbf9859e646319682
RKanimozhi04072004 <kanimozhikani68405@gmail.com>
2023-10-12 22:21:57 +0530

    first commit

---
+++ src/components/AgeCalculator.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+function AgeCalculator(){
+  const [dob,setDob]=useState('');
+  const [age,setAge]=useState(null);
+  const calculateAge=()=>{
+    const dobDate=new Date(dob);
+    const currentDate=new Date();
+    const ageInMilliSeconds=currentDate-dobDate;
+    const ageInYears=ageInMilliSeconds/(1000*60*60*24*365.25);
+    setAge(Math.floor(ageInYears));
+  };
+  return(
+    <div class="container">
+      <h1>Age Calculator</h1>
+      <h4>Enter your date of birth</h4>
+      <input
+       type="date"
+       value={dob} 
+       onChange={(e)=>setDob(e.target.value)}
+      />
+      <br>
+      </br>
+      <button onClick={calculateAge}>Calculate Age</button>
+      {age!==null && <h2>Your age is:{age} years</h2>}
+    </div>
+  );
+}
+export default AgeCalculator;
